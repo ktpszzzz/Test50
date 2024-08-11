586c4d0eefaf9953e7fc73da666f4cad6beee386
ktpszzzz <6610122113033@pnru.ac.th>
2024-08-11 12:51:10 +0700

    first commit

---
+++ T1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class T1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score = scanner.nextInt();
+        String grade ="";
+        if (score>=80) {
+            grade = "A";
+        }else{
+            if (score>=75) {
+                grade = "B+";
+            }else{
+                if (score>=70) {
+                    grade = "B";
+                }else{
+                    if (score>=65) {
+                        grade = "C+";
+                    }else{
+                        if (score>=60) {
+                            grade = "C";
+                        }else{
+                            if (score>=55) {
+                                grade = "D+";
+                            }else{
+                                if (score>=50) {
+                                    grade = "D";
+                                }else{
+                                    grade = "E";
+                                }
+
+                    }
+                }
+            }
+         }
+      }
+    }System.out.println("Your grade " + grade);
+   }
+ }
